4376cea42952aeceaf16e26042a65de1f69f2718
Omaima Assif <omaima.assif-etu@etu.univh2c.ma>
2025-06-25 00:35:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/AniGo-Animeapp/",
+  base: process.env.VITE_BASE_PATH || "/AniGo-Animeapp/",
 })
  
